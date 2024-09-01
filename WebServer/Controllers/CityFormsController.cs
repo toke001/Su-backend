b5310b1d12b7d2c31ecca162d4b30a73293c1c14
Toke <mtm201305@gmail.com>
2024-09-01 21:11:15 +0500

    CityForm controller

--- WebServer/Controllers/CityFormsController.cs
+++ WebServer/Controllers/CityFormsController.cs
@@ -123,17 +123,17 @@ namespace WebServer.Controllers
         /// <summary>
         /// Добавление формы 
         /// </summary>
-        /// <param name="idDoc"></param>
-        /// <param name="cityFormsDto"></param>
+        /// <param name="login"></param>
+        /// <param name="cityFormDto"></param>
         /// <returns></returns>
         [HttpPost("AddCityForms")]
         [Authorize]
-        public async Task<ActionResult> AddCityForms(Guid idDoc, CityFormDto cityFormsDto)
+        public async Task<ActionResult> AddCityForms(string login, List<CityFormDto> cityFormDto)
         {
             try
             {
-                var entity = _mapper.Map<CityForm>(cityFormsDto);
-                return Ok(await _repo.AddCityForms(idDoc, entity));
+                var entity = _mapper.Map<List<CityForm>>(cityFormDto);
+                return Ok(await _repo.AddCityForms(login, entity));
                 //return CreatedAtAction(nameof(GetById), new { id = entity.Id }, dto);
             }
             catch (Exception ex)
@@ -170,11 +170,12 @@ namespace WebServer.Controllers
         /// Обновление формы
         /// </summary>
         /// <param name="id"></param>
+        /// <param name="login"></param>
         /// <param name="patchDoc"></param>
         /// <returns></returns>
         [HttpPatch("{id}")]
         [Authorize]
-        public async Task<IActionResult> Patch(Guid id, [FromBody] JsonPatchDocument<CityForm> patchDoc)
+        public async Task<IActionResult> Patch(Guid id, string login, [FromBody] JsonPatchDocument<CityForm> patchDoc)
         {
             if (patchDoc == null)
             {
@@ -194,276 +195,9 @@ namespace WebServer.Controllers
                 return BadRequest(ModelState);
             }
 
-            await _repo.UpdateCityForm(entity);
+            await _repo.UpdateCityForm(login, entity);
 
             return Ok(entity);
-        }
-
-        ///// <summary>
-        ///// Получение водоснабжения
-        ///// </summary>
-        ///// <param name="idForm"></param>
-        ///// <returns></returns>
-        //[HttpGet("GetWaterSupply")]
-        //public async Task<ActionResult> GetWaterSupply(Guid idForm)
-        //{
-        //    try
-        //    {
-        //        var entity = await _repo.GetWaterSupply(idForm);
-        //        if (entity == null)
-        //        {
-        //            return NotFound();
-        //        }                
-        //        return Ok(entity);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return BadRequest(ex.Message);
-        //    }
-        //}
-
-        ///// <summary>
-        ///// Добавление водоснабжения
-        ///// </summary>
-        ///// <param name="idForm"></param>
-        ///// <param name="dto"></param>
-        ///// <returns></returns>
-        //[HttpPost("AddWaterSupply")]
-        //[Authorize]
-        //public async Task<ActionResult> AddWaterSupply(Guid idForm, CityWaterSupplyDto dto)
-        //{
-        //    try
-        //    {
-        //        var entity = _mapper.Map<CityWaterSupply>(dto);
-        //        return Ok(await _repo.AddWaterSupply(idForm, entity));
-        //        //return CreatedAtAction(nameof(GetById), new { id = entity.Id }, dto);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return BadRequest(ex.Message);
-        //    }
-        //}
-
-        ///// <summary>
-        ///// Обновление водоснабжения
-        ///// </summary>
-        ///// <param name="dto"></param>
-        ///// <returns></returns>
-        //[HttpPut("UpdateWaterSupply")]
-        //[Authorize]
-        //public async Task<ActionResult> UpdateWaterSupply(CityWaterSupplyDto dto)
-        //{
-        //    try
-        //    {
-        //        var entity = await _repo.GetWaterSupply(dto.IdForm);
-        //        if (entity == null) { return NotFound(); }
-        //        var model = _mapper.Map<CityWaterSupply>(dto);
-        //        if(model.AutoProccesNasosStanc.HasValue) entity.AutoProccesNasosStanc = model.AutoProccesNasosStanc;
-        //        if(model.AutoProccesSetVodosnab.HasValue) entity.AutoProccesSetVodosnab = model.AutoProccesSetVodosnab;
-
-
-        //        return Ok(await _repo.UpdateWaterSupply(entity));
-        //        //return CreatedAtAction(nameof(GetById), new { id = entity.Id }, dto);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return BadRequest(ex.Message);
-        //    }
-        //}       
-
-        ///// <summary>
-        ///// Получение Водоотведения
-        ///// </summary>
-        ///// <param name="idForm"></param>
-        ///// <returns></returns>
-        //[HttpGet("GetWaterDisposal")]
-        //public async Task<ActionResult> GetWaterDisposal(Guid idForm)
-        //{
-        //    try
-        //    {
-        //        var entity = await _repo.GetWaterDisposal(idForm);
-        //        if (entity == null)
-        //        {
-        //            return NotFound();
-        //        }
-        //        return Ok(entity);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return BadRequest(ex.Message);
-        //    }
-        //}
-
-        ///// <summary>
-        ///// Добавление водоотведения
-        ///// </summary>
-        ///// <param name="idForm"></param>
-        ///// <param name="dto"></param>
-        ///// <returns></returns>
-        //[HttpPost("AddWaterDisposal")]
-        //[Authorize]
-        //public async Task<ActionResult> AddWaterDisposal(Guid idForm, CityWaterDisposalDto dto)
-        //{
-        //    try
-        //    {
-        //        var entity = _mapper.Map<CityWaterDisposal>(dto);
-        //        return Ok(await _repo.AddWaterDisposal(idForm, entity));
-        //        //return CreatedAtAction(nameof(GetById), new { id = entity.Id }, dto);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return BadRequest(ex.Message);
-        //    }
-        //}
-
-        ///// <summary>
-        ///// Обновление водоотведения
-        ///// </summary>
-        ///// <param name="dto"></param>
-        ///// <returns></returns>
-        //[HttpPut("UpdateWaterDisposal")]
-        //[Authorize]
-        //public async Task<ActionResult> UpdateWaterDisposal(CityWaterDisposalDto dto)
-        //{
-        //    try
-        //    {
-        //        var entity = _mapper.Map<CityWaterDisposal>(dto);
-        //        return Ok(await _repo.UpdateWaterDisposal(entity));
-        //        //return CreatedAtAction(nameof(GetById), new { id = entity.Id }, dto);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return BadRequest(ex.Message);
-        //    }
-        //}
-
-        ///// <summary>
-        ///// Получение тарифа
-        ///// </summary>
-        ///// <param name="idForm"></param>
-        ///// <returns></returns>
-        //[HttpGet("GetTarifInfo")]
-        //public async Task<ActionResult> GetTarifInfo(Guid idForm)
-        //{
-        //    try
-        //    {
-        //        var entity = await _repo.GetTarifInfo(idForm);
-        //        if (entity == null)
-        //        {
-        //            return NotFound();
-        //        }
-        //        return Ok(entity);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return BadRequest(ex.Message);
-        //    }
-        //}
-        ///// <summary>
-        ///// Добавление тарифа
-        ///// </summary>
-        ///// <param name="idForm"></param>
-        ///// <param name="dto"></param>
-        ///// <returns></returns>
-        //[HttpPost("AddTarifInfo")]
-        //[Authorize]
-        //public async Task<ActionResult> AddTarifInfo(Guid idForm, CityTarifDto dto)
-        //{
-        //    try
-        //    {
-        //        var entity = _mapper.Map<CityTarif>(dto);
-        //        return Ok(await _repo.AddTarifInfo(idForm, entity));
-        //        //return CreatedAtAction(nameof(GetById), new { id = entity.Id }, dto);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return BadRequest(ex.Message);
-        //    }
-        //}
-
-        ///// <summary>
-        ///// Обновление тарифа
-        ///// </summary>
-        ///// <param name="dto"></param>
-        ///// <returns></returns>
-        //[HttpPut("UpdateTariffInfo")]
-        //[Authorize]
-        //public async Task<ActionResult> UpdateTariffInfo(CityTarifDto dto)
-        //{
-        //    try
-        //    {
-        //        var entity = _mapper.Map<CityTarif>(dto);
-        //        return Ok(await _repo.UpdateTariffInfo(entity));
-        //        //return CreatedAtAction(nameof(GetById), new { id = entity.Id }, dto);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return BadRequest(ex.Message);
-        //    }
-        //}
-
-        ///// <summary>
-        ///// Получение Протяженность сетей
-        ///// </summary>
-        ///// <param name="idForm"></param>
-        ///// <returns></returns>
-        //[HttpGet("GetNetworkLength")]
-        //public async Task<ActionResult> GetNetworkLength(Guid idForm)
-        //{
-        //    try
-        //    {
-        //        var entity = await _repo.GetNetworkLength(idForm);
-        //        if (entity == null)
-        //        {
-        //            return NotFound();
-        //        }
-        //        return Ok(entity);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return BadRequest(ex.Message);
-        //    }
-        //}
-
-        ///// <summary>
-        ///// Добавление Протяженность сетей
-        ///// </summary>
-        ///// <param name="idForm"></param>
-        ///// <param name="dto"></param>
-        ///// <returns></returns>
-        //[HttpPost("AddNetworkLength")]
-        //[Authorize]
-        //public async Task<ActionResult> AddNetworkLength(Guid idForm, CityNetworkLengthDto dto)
-        //{
-        //    try
-        //    {
-        //        var entity = _mapper.Map<CityNetworkLength>(dto);
-        //        return Ok(await _repo.AddNetworkLength(idForm, entity));
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return BadRequest(ex.Message);
-        //    }
-        //}
-
-        ///// <summary>
-        ///// Обновление Протяженность сетей
-        ///// </summary>
-        ///// <param name="dto"></param>
-        ///// <returns></returns>
-        //[HttpPut("UpdateNetworkLength")]
-        //[Authorize]
-        //public async Task<ActionResult> UpdateNetworkLength(CityNetworkLengthDto dto)
-        //{
-        //    try
-        //    {
-        //        var entity = _mapper.Map<CityNetworkLength>(dto);
-        //        return Ok(await _repo.UpdateNetworkLength(entity));
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return BadRequest(ex.Message);
-        //    }
-        //}
+        }        
     }
 }
--- WebServer/Reposotory/CityFormsRepository.cs
+++ WebServer/Reposotory/CityFormsRepository.cs
@@ -90,126 +90,52 @@ namespace WebServer.Reposotory
             var isReportable = await _dbSetKato.Where(x => x.Code.ToString() == kodNaselPunk).Select(x => x.IsReportable).FirstOrDefaultAsync();
             if (!isReportable) return "NotReporting";
 
-            return await _dbSetDoc.Where(x=>x.KodNaselPunk==kodNaselPunk&&x.Year==year)
-                .Select(x=>x.CityForm).FirstOrDefaultAsync();
+            var docId = await _dbSetDoc.Where(x => x.KodNaselPunk == kodNaselPunk && x.Year == year)
+                .Select(x => x.Id).FirstOrDefaultAsync();
+            var res = await _dbSetForm.Where(x => x.DocumentId == docId).ToListAsync();
+            return res;
         }
 
-        public async Task<CityForm> AddCityForms(Guid idDoc, CityForm cityForms)
+        public async Task<List<CityForm>> AddCityForms(string login, List<CityForm> cityForms)
         {
-            var doc = await _dbSetDoc.FindAsync(idDoc);
-            if (doc == null) throw new Exception("Документа с таким Id не существует");
+            var loginKatoCode = await _dbSetAccount.Where(x => x.Login == login).Select(x => x.KatoCode).FirstOrDefaultAsync();
+            var loginKato = await _dbSetKato.FirstOrDefaultAsync(x => x.Code == loginKatoCode);
+            var loginOblast = await FindParentRecordAsync(loginKato?.Id ?? 0, 0);
 
-            if (doc.CityFormId != null) throw new Exception("У данного документа уже есть форма");
+            var idDoc = cityForms.Select(x => x.DocumentId).FirstOrDefault();
+            var cityDocument = await _dbSetDoc.FindAsync(idDoc);
+            if (cityDocument == null) throw new Exception("Документа с таким Id не существует");
+            var cityDocumentKato = await _dbSetKato.FirstOrDefaultAsync(x => x.Code.ToString() == cityDocument.KodNaselPunk);
+            var cityDocumentOblast = await FindParentRecordAsync(cityDocumentKato?.Id ?? 0, 0);
 
-            await _dbSetForm.AddAsync(cityForms);
+            if (loginOblast?.Id != cityDocumentOblast?.Id) throw new Exception("Можно создавать форму только в рамках своей области!");
+
+            if (cityForms.Count == 0) throw new Exception("Формы пустые");
+
+            await _dbSetForm.AddRangeAsync(cityForms);
             await _context.SaveChangesAsync();
-
-            doc.CityFormId = cityForms.Id;
-
-            await _context.SaveChangesAsync();
-
             return cityForms;
         }
 
-        public async Task<CityForm> GetCityFormById(Guid id)
-        {
-            return await _dbSetForm.FindAsync(id);
-        }
+        public async Task<CityForm> GetCityFormById(Guid id) => await _dbSetForm.FindAsync(id);
 
-        public async Task<CityForm> UpdateCityForm(CityForm cityForm)
+        public async Task<CityForm> UpdateCityForm(string login, CityForm cityForm)
         {
+            var loginKatoCode = await _dbSetAccount.Where(x => x.Login == login).Select(x => x.KatoCode).FirstOrDefaultAsync();
+            var loginKato = await _dbSetKato.FirstOrDefaultAsync(x => x.Code == loginKatoCode);
+            var loginOblast = await FindParentRecordAsync(loginKato?.Id ?? 0, 0);
+
+            var idDoc = cityForm.DocumentId;
+            var cityDocument = await _dbSetDoc.FindAsync(idDoc);
+            if (cityDocument == null) throw new Exception("Документа с таким Id не существует");
+            var cityDocumentKato = await _dbSetKato.FirstOrDefaultAsync(x => x.Code.ToString() == cityDocument.KodNaselPunk);
+            var cityDocumentOblast = await FindParentRecordAsync(cityDocumentKato?.Id ?? 0, 0);
+
+            if (loginOblast?.Id != cityDocumentOblast?.Id) throw new Exception("Можно создавать форму только в рамках своей области!");
+
             _dbSetForm.Update(cityForm);
             await _context.SaveChangesAsync();
             return cityForm;
-        }
-
-        //public async Task<CityWaterSupply> GetWaterSupply(Guid idForm)
-        //{
-        //    return await _dbSetSupply.FirstOrDefaultAsync(x => x.IdForm == idForm);
-        //}
-
-        //public async Task<CityWaterSupply> AddWaterSupply(Guid idForm, CityWaterSupply waterSupplyInfo)
-        //{
-        //    var cityForm = await _dbSetForm.FindAsync(idForm);
-        //    if (cityForm == null) throw new Exception("Форма не найдена");
-        //    waterSupplyInfo.IdForm = idForm;
-        //    _dbSetSupply.Add(waterSupplyInfo);
-        //    await _context.SaveChangesAsync();
-        //    return waterSupplyInfo;
-        //}
-
-        //public async Task<CityWaterSupply> UpdateWaterSupply(CityWaterSupply waterSupplyInfo)
-        //{
-        //    _dbSetSupply.Update(waterSupplyInfo);
-        //    await _context.SaveChangesAsync();
-        //    return waterSupplyInfo;
-        //}
-
-        //public async Task<CityWaterDisposal> GetWaterDisposal(Guid idForm)
-        //{
-        //    return await _dbSetDisposal.FirstOrDefaultAsync(x => x.IdForm == idForm);
-        //}
-
-        //public async Task<CityWaterDisposal> AddWaterDisposal(Guid idForm, CityWaterDisposal waterDisposalInfo)
-        //{
-        //    var cityForm = await _dbSetForm.FindAsync(idForm);
-        //    if (cityForm == null) throw new Exception("Форма не найдена");
-        //    waterDisposalInfo.IdForm = idForm;
-        //    _dbSetDisposal.Add(waterDisposalInfo);
-        //    await _context.SaveChangesAsync();
-        //    return waterDisposalInfo;
-        //}
-
-        //public async Task<CityWaterDisposal> UpdateWaterDisposal(CityWaterDisposal waterDisposalInfo)
-        //{
-        //    _dbSetDisposal.Update(waterDisposalInfo);
-        //    await _context.SaveChangesAsync();
-        //    return waterDisposalInfo;
-        //}
-
-
-        //public async Task<CityTarif> GetTarifInfo(Guid idForm)
-        //{
-        //    return await _dbSetTarif.FirstOrDefaultAsync(x => x.IdForm == idForm);
-        //}
-
-        //public async Task<CityTarif> AddTarifInfo(Guid idForm, CityTarif tariffInfo)
-        //{
-        //    var cityForm = await _dbSetForm.FindAsync(idForm);
-        //    if (cityForm == null) throw new Exception("Форма не найдена");
-        //    tariffInfo.IdForm = idForm;
-        //    _dbSetTarif.Add(tariffInfo);
-        //    await _context.SaveChangesAsync();
-        //    return tariffInfo;
-        //}
-
-        //public async Task<CityTarif> UpdateTariffInfo(CityTarif tariffInfo)
-        //{
-        //    _dbSetTarif.Update(tariffInfo);
-        //    await _context.SaveChangesAsync();
-        //    return tariffInfo;
-        //}
-
-        //public async Task<CityNetworkLength> GetNetworkLength(Guid idForm)
-        //{
-        //    return await _dbSetNetwork.FirstOrDefaultAsync(x => x.IdForm == idForm);
-        //}
-
-        //public async Task<CityNetworkLength> AddNetworkLength(Guid idForm, CityNetworkLength networkLengthInfo)
-        //{
-        //    var cityForm = await _dbSetForm.FindAsync(idForm);
-        //    if (cityForm == null) throw new Exception("Форма не найдена");
-        //    networkLengthInfo.IdForm = idForm;
-        //    _dbSetNetwork.Add(networkLengthInfo);
-        //    await _context.SaveChangesAsync();
-        //    return networkLengthInfo;
-        //}
-
-        //public async Task<CityNetworkLength> UpdateNetworkLength(CityNetworkLength networkLengthInfo)
-        //{
-        //    _dbSetNetwork.Update(networkLengthInfo);
-        //    await _context.SaveChangesAsync();
-        //    return networkLengthInfo;
-        //}
+        }        
     }
 }
